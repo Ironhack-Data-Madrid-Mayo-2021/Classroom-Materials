d6911e4e9fc6f15c3f43d02a92430389007a148f
agalvezcorell <agalvezcorell@gmail.com>
2021-06-16 13:30:22 +0200

    updating

--- week_03/SQL_scripts/03_intro_joins.sql
+++ week_03/SQL_scripts/03_intro_joins.sql
@@ -1,8 +1,23 @@
 /* Al ver nuestro RD, sabemos que las editoriales y los títulos tienen una relación de uno a muchos. 
 Unámoslos y obtengamos un recuento del número de títulos que ha publicado cada editorial. */
 
+SELECT pub_name, title
+FROM publishers
+INNER JOIN titles
+ON publishers.pub_id = titles.pub_id;
+
+SELECT pub_name, titles.pub_id, COUNT(title_id) AS count_titles
+FROM publishers
+INNER JOIN titles
+ON publishers.pub_id = titles.pub_id
+GROUP BY pub_name, titles.pub_id;
 
 
+SELECT pub_name, title, COUNT(title) AS count_titles
+FROM publishers
+INNER JOIN titles
+ON publishers.pub_id = titles.pub_id
+GROUP BY pub_name, title;
 
 /* Como hemos utilizado un INNER JOIN para esta consulta, sólo devuelve los resultados de las editoriales cuyo pub_id está en ambas tablas. 
 ¿Qué pasaría si quisiéramos que los resultados devolvieran los registros de todas las editoriales, independientemente de si han publicado algún título? 
@@ -11,13 +26,28 @@ Todo lo que tenemos que hacer es sustituir nuestro INNER JOIN por un LEFT JOIN y
 de editoriales y contará con cero registros para las que no tienen ningún título publicado.*/
 
 
+SELECT pub_name, COUNT(title_id) AS count_titles
+FROM publishers
+LEFT JOIN titles
+ON publishers.pub_id = titles.pub_id
+GROUP BY pub_name;
 
+SELECT pub_name, title
+FROM publishers
+LEFT JOIN titles
+ON publishers.pub_id = titles.pub_id;
 
 
 /* ¿Y si quisiéramos analizar cuántas unidades se han vendido de cada título? 
 Podríamos declarar nuestra tabla de ventas en primer lugar, nuestra tabla de títulos en segundo lugar, 
 y utilizar un RIGHT JOIN para garantizar que nuestra consulta
 devuelva un registro para cada título (incluso los que no se vendieron). */
+
+SELECT title, SUM(qty) AS vendidos
+FROM sales
+RIGHT JOIN titles
+ON sales.title_id = titles.title_id
+GROUP BY title;
 
 
 
